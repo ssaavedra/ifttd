76fbe870e8813aabbaf6a4ab4c4d28e3444f1175
Santiago Saavedra <santiagosaavedra@gmail.com>
2012-11-11 02:01:01 +0100

    Implements (a single) transaction on the OwlService.

--- src/es/udc/choveduro/ifttd/service/OwlService.java
+++ src/es/udc/choveduro/ifttd/service/OwlService.java
@@ -1,6 +1,7 @@
 package es.udc.choveduro.ifttd.service;
 
 import java.sql.SQLException;
+import java.util.ArrayList;
 
 import android.content.Intent;
 import android.os.Binder;
@@ -11,6 +12,8 @@ import android.widget.Toast;
 import com.j256.ormlite.android.apptools.OrmLiteBaseService;
 import com.j256.ormlite.dao.Dao;
 
+import es.udc.choveduro.ifttd.conditions.OnTimeCondition;
+import es.udc.choveduro.ifttd.consequences.ShowNotification;
 import es.udc.choveduro.ifttd.types.Accion;
 import es.udc.choveduro.ifttd.types.Condition;
 import es.udc.choveduro.ifttd.types.Consequence;
@@ -19,8 +22,15 @@ public class OwlService extends OrmLiteBaseService<DatabaseHelper> {
 	private static final String TAG = "IFTTD-OwlService";
 
 	IBinder mBinder = new OwlBinder();
-	
+
 	DatabaseHelper dbh;
+
+	ArrayList<Condition> conditionCache = new ArrayList<Condition>();
+	ArrayList<Consequence> consequenceCache = new ArrayList<Consequence>();
+	Condition transactionCond;
+	Consequence transactionConsequence;
+	Accion transactionAction;
+	boolean transactionInProcess = false;
 
 	public class OwlBinder extends Binder {
 		public OwlService getService() {
@@ -33,6 +43,8 @@ public class OwlService extends OrmLiteBaseService<DatabaseHelper> {
 		super.onCreate();
 		dbh = new DatabaseHelper(this);
 		Log.i(TAG, "Creating service");
+		conditionCache.add(new OnTimeCondition());
+		consequenceCache.add(new ShowNotification());
 	}
 
 	@Override
@@ -46,22 +58,80 @@ public class OwlService extends OrmLiteBaseService<DatabaseHelper> {
 	public IBinder onBind(Intent arg0) {
 		return mBinder;
 	}
-	
+
 	public int onStartCommand(Intent intent, int flags, int startId) {
 		Toast.makeText(this, "Service started.", Toast.LENGTH_SHORT).show();
 		return START_STICKY;
 	}
-	
+
 	public Dao<Accion, Integer> getAccionDao() throws SQLException {
 		return dbh.getAccionDao();
 	}
-	
+
 	public Dao<Condition, Accion> getConditionDao() throws SQLException {
 		return dbh.getConditionDao();
 	}
-	
+
 	public Dao<Consequence, Accion> getConsequenceDao() throws SQLException {
 		return dbh.getConsequenceDao();
 	}
 
+	public ArrayList<Condition> getConditions() {
+		return conditionCache;
+	}
+	
+	public ArrayList<Consequence> getConsequences() {
+		return consequenceCache;
+	}
+	
+	public void startTransaction() {
+		transactionAction = new Accion();
+		transactionInProcess = true;
+	}
+
+	public void setTransactionCondition(Condition c) {
+		transactionCond = c;
+	}
+
+	public void setTransactionConsequence(Consequence c) {
+		transactionConsequence = c;
+	}
+
+	public void unwind() {
+		transactionCond = null;
+		transactionConsequence = null;
+		transactionAction = null;
+		transactionInProcess = false;
+	}
+
+	public void commitTransaction() {
+		transactionAction = new Accion();
+		transactionAction.setConsec(transactionConsequence);
+		transactionAction.setCond(transactionCond);
+		transactionCond.setAction(transactionAction);
+		transactionConsequence.setAction(transactionAction);
+		try {
+			dbh.getAccionDao().create(transactionAction);
+			dbh.getConditionDao().create(transactionCond);
+			dbh.getConsequenceDao().create(transactionConsequence);
+		} catch (SQLException e) {
+			Log.e(TAG, "SQL Exception when creating things...", e);
+
+			try {
+				dbh.getAccionDao().delete(transactionAction);
+				dbh.getConditionDao().delete(transactionCond);
+				dbh.getConsequenceDao().delete(transactionConsequence);
+			} catch (SQLException e1) {
+			} finally {
+				Log.e(TAG, "SQL Exception again on deletion.");
+				consequenceCache.remove(transactionConsequence);
+				try {
+					// Create new instances on the cache of the same type ;-)
+					consequenceCache.add(transactionConsequence.getClass().newInstance());
+					conditionCache.add(transactionCond.getClass().newInstance());
+				} catch (Exception e1) {
+				}
+			}
+		}
+	}
 }
